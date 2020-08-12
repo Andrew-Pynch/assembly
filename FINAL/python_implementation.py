48a729e53a02eb347f85305062706b64bc982784
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-08-12 12:34:08 -0700

    Add python implementation

--- FINAL/python_implementation.py
+++ FINAL/python_implementation.py
@@ -1,17 +1,26 @@
-def encrypt():
-    key = list("efbcdghijklmnopqrstuvwxyza")
-    alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    string = list("the contents of this message will be a mystery")
+ALPHABET = list("abcdefghijklmnopqrstuvwxyz")
+CORRECT_ENCRYPTION = "uid bpoudout pg uijt ndttehd xjmm fd e nztudsz"
 
-    print(f'ORIGINAL: {"".join(string)}')
-    for i, schar in enumerate(string):
-        for j, achar in enumerate(alphabet):
+
+def encrypt(input_string, key):
+    print(f'ORIGINAL: {"".join(input_string)}\n')
+    for i, schar in enumerate(input_string):
+        for j, achar in enumerate(ALPHABET):
             if schar == achar:
-                string[i] = key[j]
-    return "".join(string)
+                input_string[i] = key[j]
+    potential_encryption = "".join(input_string)
+
+    print(f"RESULT:  {potential_encryption}")
+    print(f"CORRECT: {CORRECT_ENCRYPTION}")
+    if potential_encryption == CORRECT_ENCRYPTION:
+        print("Correct encryption performed!")
+    else:
+        print("Incorrect encryption performed.")
+    return "".join(input_string)
 
 
 if __name__ == "__main__":
-    print(encrypt())
-    print("uid bpoudout pg uijt ndttehd xjmm fd e nztudsz")
+    key = list("efbcdghijklmnopqrstuvwxyza")
+    input_string = list("the contents of this message will be a mystery")
+    encrypt(input_string, key)
 
